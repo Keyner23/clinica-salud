6734d8552869a4f2a3feb1f4ca5954e245c263e8
keyner barrios <keinerba.ochoa@gmail.com>
2025-10-20 19:34:19 -0500

    wip

--- Repositories/AppointmentRepository.cs
+++ Repositories/AppointmentRepository.cs
@@ -2,28 +2,28 @@ using clinica_salud.Db;
 using clinica_salud.Interfaces;
 using clinica_salud.Models;
 
-namespace clinica_salud.Repositories
+namespace clinica_salud.Repositories;
+
+public class AppointmentRepository : IAppoimentRepository
 {
-    public class AppointmentRepository : IAppoimentRepository
+    private readonly List<Appointment> appointments = new();
+
+    public void AddAppoiment(Appointment appointment)
     {
-        private readonly List<Appointment> appointments = new();
-
-        public void AddAppoiment(Appointment appointment)
-        {
-            appointments.Add(appointment);
-        }
-
-        public List<Appointment> GetAllAppoiments()
-        {
-            return appointments;
-        }
-        public List<Appointment> GetAppointmentsByPetName(string petName)
-        {
-            return Database.appoiments
-                .Where(a => a.pets.Any(p => p.name.Equals(petName, StringComparison.OrdinalIgnoreCase)))
-                .ToList();
-        }
+        appointments.Add(appointment);
     }
-};
+
+    public List<Appointment> GetAllAppoiments()
+    {
+        return appointments;
+    }
+    public List<Appointment> GetAppointmentsByPetName(string petName)
+    {
+        return Database.appoiments
+            .Where(a => a.pets.Any(p => p.name.Equals(petName, StringComparison.OrdinalIgnoreCase)))
+            .ToList();
+    }
+}
+
 
 
